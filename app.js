277f79d917b7195f0227fe6777f0667b2e0be051
u60450856 <vc1983@mail.ru>
2018-12-24 22:24:24 +0300

    fix

--- app.js
+++ app.js
@@ -189,11 +189,6 @@ let APP = (function(init) {
           item=>item.getAttribute('data-illness')
         );
     console.log(t,arrSelectedIllnesses);
-
-
-
-
-
     //Считаем шансы диагностики для каждого выбранного заболенвания с каждым diagSet 
     //calculateDiag()
     const bitmaskRange = function(til){ let x = 0, xs = []; while (x < til){ xs.push(x++); }; return xs; };
