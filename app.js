79dbc4546ef6fbae4344099e6268adb7e4a23d06
u60450856 <vc1983@mail.ru>
2018-12-25 22:33:41 +0300

    debug

--- app.js
+++ app.js
@@ -207,7 +207,13 @@ let APP = (function(init) {
             }
           }
         );
-    console.log(arrSelectedIllnesses);
+    console.log(arrSelectedIllnesses);  
+
+
+
+
+
+    
     //Считаем шансы диагностики для каждого выбранного заболенвания с каждым diagSet 
     //calculateDiag()
     const bitmaskRange = function(til){ let x = 0, xs = []; while (x < til){ xs.push(x++); }; return xs; };
