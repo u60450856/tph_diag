bff7ce38b7cee8e3fcf3d136c5b51eddc784b112
u60450856 <vc1983@mail.ru>
2018-12-30 17:11:04 +0300

    debug

--- app.js
+++ app.js
@@ -332,6 +332,9 @@ let APP = (function(init) {
    _showDiagSetList(_themeDiagSetList(t2));
   };
 
+
+
+
   const _cmdClear = function (ev) { };
   const _cmdIllnessSelect = function (ev) {
        if(ev.target.classList.contains('illness')){ 
